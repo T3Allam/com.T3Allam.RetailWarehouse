5ccaf002b1647e44fc752435229f2b53d4c7356e
Allam <trkallam@gmail.com>
2019-12-18 21:21:27 -0500

    First commit - longitude & latitude

---
+++ src/main/java/com/github/T3Allam/warehouse/Client.java
@@ -0,0 +1,25 @@
+package com.github.T3Allam.warehouse;
+
+import com.byteowls.jopencage.JOpenCageGeocoder;
+import com.byteowls.jopencage.model.JOpenCageForwardRequest;
+import com.byteowls.jopencage.model.JOpenCageLatLng;
+import com.byteowls.jopencage.model.JOpenCageResponse;
+import org.apache.log4j.BasicConfigurator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Client.class);
+        BasicConfigurator.configure();
+        JOpenCageGeocoder jOpenCageGeocoder = new JOpenCageGeocoder("");
+        JOpenCageForwardRequest request = new JOpenCageForwardRequest("685 Markham St, M6G2M2, Toronto, ON, Canada");
+        request.setLimit(1);
+        request.setNoAnnotations(true);
+        JOpenCageResponse response = jOpenCageGeocoder.forward(request);
+        JOpenCageLatLng firstResultLatLng = response.getFirstPosition(); // get the coordinate pair of the first result
+        System.out.println(firstResultLatLng.getLat().toString() + "," + firstResultLatLng.getLng().toString());
+    }
+}
