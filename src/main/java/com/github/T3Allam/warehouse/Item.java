b0aa92e9074a2bbfe684e2cfffa6c1cb1f7660d1
Allam <trkallam@gmail.com>
2019-12-21 18:25:50 -0500

    Correcting Item class - removing Singleton pattern

--- src/main/java/com/github/T3Allam/warehouse/Item.java
+++ src/main/java/com/github/T3Allam/warehouse/Item.java
@@ -1,22 +1,14 @@
 package com.github.T3Allam.warehouse;
 
-//Singleton Design Pattern
+
 public class Item {
-    private static String name;
-    private static String sku;
-    private static double price;
-    private static Item uniqueInstance = null;
+    String name;
+    String sku;
+    double price;
 
     private Item(String name, String sku, double price){
         this.name = name;
         this.sku = sku;
         this.price = price;
-    }
-
-    public static Item getInstance() {
-        if (uniqueInstance == null) {
-            uniqueInstance = new Item(Item.name, Item.sku, Item.price);
-        }
-        return uniqueInstance;
     }
 }
